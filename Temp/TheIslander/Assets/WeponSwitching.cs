dbbc2d898bd1392523f3c816e944a86bed528834
Quar12 <kaldurklaki@gmail.com>
2014-01-24 17:31:39 +0000

    hreinsaði og bætti við vopnum

---
+++ Temp/TheIslander/Assets/WeponSwitching.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeponSwitching : MonoBehaviour {
+
+	public GameObject weapon1, weapon2;
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q))
+	    {
+			SwitchWepons();
+		}
+	}
+
+	void SwitchWepons()
+	{
+		if(weapon1.active)
+		{
+			weapon1.SetActive(false);
+			weapon2.SetActive(true);
+		}else
+		{
+			weapon1.SetActive(true);
+			weapon2.SetActive(false);
+		}
+	}
+}
